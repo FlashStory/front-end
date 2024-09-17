5d5d533fdb603a7fcbcb04314a7431e6fa100da6
hieuminh65 <hieuminhnguyen@usf.edu>
2024-09-17 16:11:59 -0400

    Initial Commit

---
+++ Flash Story/Flash_StoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flash_StoryApp.swift
+//  Flash Story
+//
+//  Created by Hiếu Nguyễn Minh on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Flash_StoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
